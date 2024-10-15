107bb13c758981e672572e5739b9eb8a2783f820
jimbucktwo <92623786+jimbucktwo@users.noreply.github.com>
2024-10-15 16:40:45 -0700

    Completed the project

--- ch09-proj2.js
+++ ch09-proj2.js
@@ -3,6 +3,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     const data = JSON.parse(content);
     const parent = document.querySelector("ul");
+    const parent2 = document.querySelector("figure");
 
     for (let d of data) {
         const element = document.createElement("li");
@@ -25,8 +26,31 @@ document.addEventListener("DOMContentLoaded", function () {
                 if (d.id == e.target.dataset.id) {
                     document.querySelector("#title").innerHTML = d.title;
                     document.querySelector("#artist").innerHTML = 'By ' + d.artist;
+                    
+                    for (let f of d.features) {
+                        const desc = document.createElement("div");
+                        desc.className = "box";
+                        //desc.innerHTML = '<p>' + f.description + '</p>';
+                        figure.appendChild(desc);
+                        desc.style.position = "absolute";
+                        desc.style.left = f.upperLeft[0] + 'px';
+                        desc.style.top = f.upperLeft[1] + 'px';
+                        desc.style.width = (f.lowerRight[0] - f.upperLeft[0]) + 'px';
+                        desc.style.height = (f.lowerRight[1] - f.upperLeft[1]) + 'px';
+
+                        desc.addEventListener("mouseover", function (){
+                            document.querySelector("#description").textContent = f.description;
+                        })
+
+                        desc.addEventListener("mouseout", function (){
+                            document.querySelector("#description").textContent = '';
+                        })
+                    }
+
+
                 }
             }
         }
     });
+
 });
